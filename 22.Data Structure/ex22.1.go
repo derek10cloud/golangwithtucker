b94be4e8265595ce8adc6792cbebe69a66925038
derek <derek@daangn.com>
2022-02-06 03:01:30 +0900

    자료구조 예제 추가-2

--- 22.Data Structure/ex22.1.go
+++ 22.Data Structure/ex22.1.go
@@ -6,18 +6,18 @@ import (
 )
 
 func main() {
-	v := list.New()
-	e4 := v.PushBack(4)
-	e1 := v.PushFront(1)
-	v.InsertBefore(3, e4)
-	v.InsertAfter(2, e1)
+	v := list.New()       //리스트 생성
+	e4 := v.PushBack(4)   //리스트 맨 뒤에 요소 추가
+	e1 := v.PushFront(1)  //리스트 맨 앞에 요소 추가
+	v.InsertBefore(3, e4) //e4 요소 앞에 요소 삽입
+	v.InsertAfter(2, e1)  //e1 요소 뒤에 요소 삽입
 
-	for e := v.Front(); e != nil; e = e.Next() {
+	for e := v.Front(); e != nil; e = e.Next() { //각 요소 순회
 		fmt.Print(e.Value, " ")
 	}
 
 	fmt.Println()
-	for e := v.Back(); e != nil; e = e.Prev() {
+	for e := v.Back(); e != nil; e = e.Prev() { //각 요소 역 순회
 		fmt.Print(e.Value, " ")
 	}
 	fmt.Println()
--- 22.Data Structure/ex22.2.go
+++ 22.Data Structure/ex22.2.go
@@ -13,7 +13,7 @@ func (q *Queue) Push(val interface{}) { //요소를 추가하는 메서드
 	q.v.PushBack(val)
 }
 
-func (q *Queue) Pop() interface{} {
+func (q *Queue) Pop() interface{} { //요소 반환하면서 삭제하는 메서드
 	front := q.v.Front()
 	if front != nil {
 		return q.v.Remove(front)
@@ -21,20 +21,20 @@ func (q *Queue) Pop() interface{} {
 	return nil
 }
 
-func NewQueue() *Queue {
+func NewQueue() *Queue { //새로운 리스트를 만들어서 Queue 구조체에 저장하는 함수
 	return &Queue{list.New()}
 }
 
 func main() {
-	queue := NewQueue()
+	queue := NewQueue() //새로운 큐 생성
 
-	for i := 1; i < 5; i++ {
+	for i := 1; i < 5; i++ { //요소 뒤에서 입력하는 메서드 호출
 		queue.Push(i)
 	}
-	v := queue.Pop()
+	v := queue.Pop() //요소 하나 반환하고 삭제하는 메서드 호출 (1번째)
 	for v != nil {
 		fmt.Printf("%v -> ", v)
-		v = queue.Pop()
+		v = queue.Pop() //요소 하나 반환하고 삭제하는 메서드 호출 순회 (2번째~마지막)
 	}
 	fmt.Println()
 }
